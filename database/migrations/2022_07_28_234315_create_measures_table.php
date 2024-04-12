c10c20cdd7a58e41643074d9c1012c3b753c96bd
Ignacio Fassini <nachofassini@gmail.com>
2024-04-11 22:19:00 -0300

    Update measures table

--- database/migrations/2022_07_28_234315_create_measures_table.php
+++ database/migrations/2022_07_28_234315_create_measures_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -15,14 +16,14 @@ return new class () extends Migration {
         Schema::create('measures', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->double('consumption', 4, 1);
-            $table->double('inside_temperature', 4, 1);
-            $table->double('outside_temperature', 4, 1);
-            $table->double('inside_humidity', 4, 1);
-            $table->double('outside_humidity', 4, 1);
-            $table->double('soil_humidity', 4, 1);
-            $table->unsignedSmallInteger('co2');
-            $table->double('lighting', 4, 1);
+            $table->double('consumption', 6, 2)->nullable();;
+            $table->double('inside_temperature', 6, 2);
+            $table->double('outside_temperature', 6, 2);
+            $table->double('inside_humidity', 6, 2);
+            $table->double('outside_humidity', 6, 2);
+            $table->double('soil_humidity', 6, 2);
+            $table->unsignedSmallInteger('co2')->nullable();
+            $table->double('lighting', 6, 2);
         });
     }
 
