4825b8745032c6dbd7e66ed50f5c66faef175c37
Ignacio Fassini <nachofassini@gmail.com>
2024-04-15 04:04:17 -0300

    Try new driver for dht22 sensors

--- app/Console/Commands/Sensors/lib/dht-22-external.py
+++ app/Console/Commands/Sensors/lib/dht-22-external.py
@@ -1,14 +1,26 @@
-import Adafruit_DHT
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-# Sensor type
-sensor = Adafruit_DHT.DHT22
+import adafruit_dht
+import board
 
-# GPIO pin number
-pin = 5
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D5)
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-if humidity is not None and temperature is not None:
+try:
+    # Print the values to the serial port
+    temperature = dhtDevice.temperature
+    humidity = dhtDevice.humidity
     print(f"Temp={temperature:0.1f}*C  Humidity={humidity:0.1f}%")
-else:
-    print("Failed to retrieve data from sensor")
+
+except RuntimeError as error:
+    # Errors happen fairly often, DHT's are hard to read, just keep going
+    print(error.args[0])
+except Exception as error:
+    dhtDevice.exit()
+    raise error
